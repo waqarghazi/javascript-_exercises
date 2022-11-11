5d137ad24eebf24e5a97645f8d5fddc22083faa5
Waqar Ghazi <syedmohammad76@gmail.com>
2022-11-11 09:37:53 +0500

    minor changes

--- task_21.mjs
+++ task_21.mjs
@@ -1,11 +1,13 @@
 // They think of something you could store in a JavaScript Object. Write a program that creates Objects containing these items
 
 function Car(make, model, year, owner) {
-    this.make = make;
-    this.model = model;
-    this.year = year;
-    this.owner = owner;
-  }
+    const myObj = new Object();
+    myObj.Make = make;
+    myObj.Model = model;
+    myObj.Year = year;
+    myObj.Owner = owner;
+    return myObj;
+}
 
 const car1 = new Car("Audi", "R8", 2022, "Waqar Ghazi");
 const car2 = new Car("BMW", "M4", 2022, "Shahroz");
